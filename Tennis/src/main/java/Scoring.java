8dd7b5c5a67cabd9a216cf8d331e550b86c52ea9
Thedeadz0ne <zlatkomilushev302@gmail.com>
2023-08-12 15:44:10 +0300

    Initial commit

---
+++ Tennis/src/main/java/Scoring.java
@@ -0,0 +1,8 @@
+public class Scoring {
+
+    public static void main(String[] args) {
+
+        // We want a tennis counting tool, where we can input which player scored the last point and the application prints the new score into the console.
+
+    }
+}
